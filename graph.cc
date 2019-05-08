3e4727494eb8c52b197eaca688c06f36b37601aa
Sam Field <spl.field88@gmail.com>
2019-05-08 13:12:07 -0700

    Added graph class and main file

---
+++ graph.cc
@@ -0,0 +1,56 @@
+using namespace std;
+
+class vertex
+{
+private:
+    /* data */
+public:
+    vertex(/* args */);
+    ~vertex();
+};
+
+vertex::vertex(/* args */)
+{
+}
+
+vertex::~vertex()
+{
+}
+
+
+
+class edge
+{
+private:
+    /* data */
+public:
+    edge(/* args */);
+    ~edge();
+};
+
+edge::edge(/* args */)
+{
+}
+
+edge::~edge()
+{
+}
+
+
+
+class graph
+{
+private:
+    /* data */
+public:
+    graph(/* args */);
+    ~graph();
+};
+
+graph::graph(/* args */)
+{
+}
+
+graph::~graph()
+{
+}
---
+++ main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <graph.hh>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
+
